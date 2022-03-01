0186c2a025d10def1fee5bfae8781ce35c2cd449
mpitscheider <97949136+mpitscheider@users.noreply.github.com>
2022-03-01 10:13:36 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,36 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
 #define Dateiname_MAX 20
 
-/*
-int main()
-{
-   FILE *fp;
-    int i;
-    int gel;
 
-    // oeffnen im Lesemodus
-    fp = fopen("zahlen.txt", "r");
-
-    if(fp == NULL)
-    {
-	printf("Datei konnte nicht geoeffnet werden.\n");
-    }
-    else
-    {
-        for(i=0; i<11; i++)
-        {
-
-            fscanf(fp, "%d\n", &gel);
-            printf("gelesen: %d\n", gel);
-        }
-	fclose(fp);
-    }
-
-    return 0;
-}
-*/
 
 int main()
 {
@@ -38,10 +12,14 @@ int main()
     char cdatei[Dateiname_MAX];                                         //Array für von User eingegebenen Dateinamen
     char crd[100];                                                      //"c read_data" Array für Speicherung von eingelesene Zeichen von Datei
     int n= 0;                                                           //für Index in "cd"
-
+    int a= 0;                                                           //Benutzereingabe für Verschlüsseln/Entschlüsseln
+    int va=0;                                                           //Bestimmt ob Verschlüsselt oder Entschlüsselt wird
     //###Benutzereingabe Datei###
-
-    printf("!!!zu verschluesselnde Datei muss im selben Ordner wie Code sein!!! \n");
+    printf("                  $\n                  $$\n                  $$$\n                $$$$$\n              $$$$$\n           $$$$$$$\n          $$$$$$$$$\n         $$$$$$$$$$\n        $$$(O)$(O)$$\n        $$$$$$$$$$$$\n   $$$  $$$$$( )$$$$  $$$         $\n    $$$  $$$$$$$$$$  $$$         $$\n     $$$  $$$$$$$$  $$$        $$$\n       $$$$$$$$$$$$$$         $$$$\n       $$$$$$$$$$$$$$          $$$$\n        $$$$$$$$$$$$$$$     $$$$$$\n          $$$$$$$$$$$$$$$$$$$$$$$\n             $$$$$$$$$$$$$$$$$$$\n                $$$$$$$$$$$$$\n");
+    printf("\n");
+    printf("Welcome to Ghostcypher!\n");
+    printf("\n");
+    printf("!!!zu verschluesselnde/entschluesselnde Datei muss im selben Ordner wie Code sein!!! \n");
     printf("Dateiname: ");
     //Benutzereingabe
     fgets(cdatei, Dateiname_MAX, stdin);                                //In welches Array speichern, max. Anzahl an Zeichen, Eingabetyp: Standardeingabe
@@ -59,22 +37,30 @@ int main()
         //Zeichen einlesen und in char speichern
         //###Leerzeichen und Zeilenumbrüche -> nichts (Leerzeichen) wird ausgegeben###
         //"(rd[0] = fgetc(fp))!= EOF" =eingelesenes Zeichen in Array "crd"() speichern und abfragen ob EOF (=End Of File) erreicht ist
+        printf("Verschluesseln[1], Entschluesseln[2]: ");
+        scanf ("%3d", &a);
+
+
+        //
+    if(a==1) va=12;
+    else if(a==2) va=40;
+    else    {printf("Falsche Eingabe");
+            return 0;}
+
+        printf("Verschluesselter/Entschluesselter Text: ");
         while ((crd[n] = fgetc(fp))!= EOF)
         {
-            printf("gelesen: %c \n", crd[n]);
-            n++;
+
+            if (!isspace(crd[n]) || !isblank(crd[n]))
+            printf("%c", (((crd[n] - 97) + va) % 26) + 97);
+        else
+            printf("%c", crd[n]);
+        crd[n]+= 1;
+
         }
-
-        //gibt komplett eingelesene Datei aus
-        printf("\n%s \n", crd);
-
-    }
+}
 
     fclose(fp);
 
     return 0;
 }
-
-
-
-
