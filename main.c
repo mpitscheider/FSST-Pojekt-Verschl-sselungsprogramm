d0ce30958b1a345bcc0e8b3d2dd89c995826ad22
mpitscheider <97949136+mpitscheider@users.noreply.github.com>
2022-03-29 09:33:28 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,66 +1,163 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#define Dateiname_MAX 20
+#include <conio.h>
+#include "ghost.h"
+
+int b;
+int a;
+int go;
+int h=1;
+int vcode;
+int cont=0;
+char in[260];
+char name[256];
+char name1[256];
 
 
-
-int main()
+void infos()
 {
-    FILE *fp;                                                           //erstellt Pointer (Name= fp) für Files
-    char cdatei[Dateiname_MAX];                                         //Array für von User eingegebenen Dateinamen
-    char crd[100];                                                      //"c read_data" Array für Speicherung von eingelesene Zeichen von Datei
-    int n= 0;                                                           //für Index in "cd"
-    int a= 0;                                                           //Benutzereingabe für Verschlüsseln/Entschlüsseln
-    int va=0;                                                           //Bestimmt ob Verschlüsselt oder Entschlüsselt wird
-    //###Benutzereingabe Datei###
-    printf("                  $\n                  $$\n                  $$$\n                $$$$$\n              $$$$$\n           $$$$$$$\n          $$$$$$$$$\n         $$$$$$$$$$\n        $$$(O)$(O)$$\n        $$$$$$$$$$$$\n   $$$  $$$$$( )$$$$  $$$         $\n    $$$  $$$$$$$$$$  $$$         $$\n     $$$  $$$$$$$$  $$$        $$$\n       $$$$$$$$$$$$$$         $$$$\n       $$$$$$$$$$$$$$          $$$$\n        $$$$$$$$$$$$$$$     $$$$$$\n          $$$$$$$$$$$$$$$$$$$$$$$\n             $$$$$$$$$$$$$$$$$$$\n                $$$$$$$$$$$$$\n");
-    printf("\n");
-    printf("Welcome to Ghostcypher!\n");
-    printf("\n");
-    printf("!!!zu verschluesselnde/entschluesselnde Datei muss im selben Ordner wie Code sein!!! \n");
-    printf("Dateiname: ");
-    //Benutzereingabe
-    fgets(cdatei, Dateiname_MAX, stdin);                                //In welches Array speichern, max. Anzahl an Zeichen, Eingabetyp: Standardeingabe
-    cdatei[strlen(cdatei) -1] = '\0';                                   //automatisches Leerzeichen (am Ende von get-Eingabe) durch 0 ersetzen
-    printf("Datei: %s \n", cdatei);
-    //Zeiger auf zu öffnende Datei
-    fp= fopen(cdatei, "r");
+        printf("\n");
+        printf("Information:\n");
+        printf("With Ghostcypher you can decrypt/encrypt any type of files!\n");
+        printf("To decrypt the files, the encryption code is required which was previously entered during \n");
+        printf("the encryption process. If these codes are not the same, the file remains encrypted.\n");
+        printf("The files encrypted/decrypted with Ghostcypher are saved under a name of your choice, \n");
+        printf("in the same folder as the program.\n");
+        printf("-> It's important that you save the decrypted File with the same Ending as the encrypted one!!");
+        printf("\n");
+        printf("\n");
+        printf("To start type [1]");
+        scanf("%d", &go);
 
-    if (fp== NULL)
+        if(go==1)
+        {
+            start();
+        }
+        else
+        {
+            return 0;
+        }
+
+}
+
+void start()
+{
+    system("COLOR E");
+    printf("Which action should be carried out?\n");
+    printf("Encrypt [1], Decrypt [2]: ");
+    scanf("%d", &a);
+    if (a!=2 && a!=1 )
     {
-        printf("Datei konnte nicht geoeffnet werden!");
+        printf("ERROR: wrong Input!");
+        return 0;
     }
+
     else
     {
-        //Zeichen einlesen und in char speichern
-        //###Leerzeichen und Zeilenumbrüche -> nichts (Leerzeichen) wird ausgegeben###
-        //"(rd[0] = fgetc(fp))!= EOF" =eingelesenes Zeichen in Array "crd"() speichern und abfragen ob EOF (=End Of File) erreicht ist
-        printf("Verschluesseln[1], Entschluesseln[2]: ");
-        scanf ("%3d", &a);
-
-
-        //
-    if(a==1) va=12;
-    else if(a==2) va=40;
-    else    {printf("Falsche Eingabe");
-            return 0;}
-
-        printf("Verschluesselter/Entschluesselter Text: ");
-        while ((crd[n] = fgetc(fp))!= EOF)
+        printf("\n");
+        printf("-------------------------------------------------------------------------------------\n");
+        printf("  IMPORTANT: \n");
+        printf("   -The file to be encrypted/decrypted must be in the same folder as the code\n");
+        printf("   -Full file name must be entered\n");
+        printf("-------------------------------------------------------------------------------------\n");
+        printf("\n");
+        printf("Filename: ");
+        scanf("%s", &in);
+        printf("IMPORTANT: The encryption-code must be the same for both encryption and decryption.!\n");
+        printf("Encryption-Code: ");
+        scanf("%d", &vcode);
+        if (a == 1)
         {
+            char c;
+            FILE *fin;
+            FILE *fout;
+            fin = fopen(in, "rb");
+            printf("Filename of the encrypted file: ");
+            scanf("%s", &name);
+            strcpy(in, name);
+            printf("\n");
+            printf("Successfully encrypted!");
+            fout = fopen(in, "wb");
+            while (!feof(fin))
+            {
+                c = fgetc(fin);
+                int temp = (c + vcode);
+                fputc((char)temp, fout);
+            }
+            fclose(fin);
+            fclose(fout);
+        }
+        else if (a == 2)
+        {
+            char c;
+            FILE *fin;
+            FILE *fout;
+            fin = fopen(in, "rb");
+            printf("Filename of the decrypted file: ");
+            scanf("%s", &name1);
+            strcpy(in, name1);
+            printf("\n");
+            printf("Successfully decrypted!");
+            fout = fopen(in, "wb");
+            while (!feof(fin))
+            {
+                c = fgetc(fin);
+                int temp = (c - vcode);
+                fputc((char)temp, fout);
+            }
 
-            if (!isspace(crd[n]) || !isblank(crd[n]))
-            printf("%c", (((crd[n] - 97) + va) % 26) + 97);
-        else
-            printf("%c", crd[n]);
-        crd[n]+= 1;
-
+        fclose(fin);
+        fclose(fout);
+    }
+    }
+    getch();
+}
+void con()
+{
+        printf("\n\nClose Ghostcypher[1]\n");
+        printf("Run again?[2]\n");
+        scanf("%d", &cont);
+        if (cont==2)
+        {
+            start();
+        }
+        else if (cont!=2)
+        {
+            h=0;
+            return 0;
         }
 }
 
-    fclose(fp);
 
-    return 0;
+int main ()
+{
+    system("COLOR E");
+    ghost();
+    printf("\n");
+    printf("          Welcome to Ghostcypher!\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("To start type [1]\n");
+    printf("For information type [2]\n");
+    scanf("%d", &b);
+    if(b==2)
+    {
+        infos();
+    }
+    if(b==1)
+    {
+        start();
+        con();
+        while (h==1)
+        {
+        con();
+        }
+    }
+    else
+    {
+        printf("ERROR: wrong Input!");
+        return 0;
+    }
+
 }
